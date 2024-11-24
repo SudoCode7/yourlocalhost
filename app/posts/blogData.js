bee8135aa03e75c51436dd5b46e232b29c4d8169
SoduCode7 <jakshat70@gmail.com>
2024-11-24 13:55:51 +0530

    1st sweep

--- app/posts/blogData.js
+++ app/posts/blogData.js
@@ -1,16 +1,16 @@
 export default [
   {
     id: 1,
-    slug: 'introduction-to-javascript',
+    slug: 'rev-engg-wifi-router',
     blogFile: '/blog1.md',
     date: '2024-01-15',
-    title: 'A Comprehensive Guide to Getting Started with JavaScript for Beginners',
+    title: 'My Hands-on Reverse Engineering a Router... Everyone is welcome',
   },
-  {
-    id: 2,
-    slug: 'mastering-react-hooks',
-    blogFile: '/blog2.md',
-    date: '2024-02-10',
-    title: 'Mastering React Hooks: A Complete Guide to Efficient State Management',
-  },
+  // {
+  //   id: 2,
+  //   slug: 'mastering-react-hooks',
+  //   blogFile: '/blog2.md',
+  //   date: '2024-02-10',
+  //   title: 'Mastering React Hooks: A Complete Guide to Efficient State Management',
+  // },
 ];
--- public/media.md
+++ public/media.md
@@ -1,6 +1,6 @@
 # MEDIA / TALKS
 
 ## Media:
-### (ISACA’s Cybersecurity Scholarship Recipient)[https://www.isaca.org/resources/news-and-trends/isaca-now-blog/2024/celebrating-isacas-cybersecurity-month-scholarship-recipients]: 
+### [ISACA’s Cybersecurity Scholarship Recipient](https://www.isaca.org/resources/news-and-trends/isaca-now-blog/2024/celebrating-isacas-cybersecurity-month-scholarship-recipients): 
 The ISACA Foundation offers annual Cybersecurity Month Scholarships to support global students pursuing undergraduate or graduate degrees in cybersecurity. The scolarship awardees belong to various cultural backgrounds and are awarded based on their social as well as academia contributions.
 
